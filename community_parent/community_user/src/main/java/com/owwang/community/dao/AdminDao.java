9d772582f6408b5f8a65bc5ec566e0174cd9e6c3
isamuelwang <isamuelwang@gmail.com>
2020-01-12 10:48:54 +0800

    增加建表语句

---
+++ community_parent/community_user/src/main/java/com/owwang/community/dao/AdminDao.java
@@ -0,0 +1,14 @@
+package com.owwang.community.user.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import com.owwang.community.user.pojo.Admin;
+/**
+ * 数据访问接口
+ * @author Administrator
+ *
+ */
+public interface AdminDao extends JpaRepository<Admin,String>,JpaSpecificationExecutor<Admin>{
+	
+}
